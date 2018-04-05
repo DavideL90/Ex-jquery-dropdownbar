493c3eb218b1dca621e529ad3a7b475a7c09913e
DavideL90 <davide.lecci@icloud.com>
2018-04-05 17:11:06 +0200

    es con zindex non funzionante e secondo dropdown da sistemare

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
   $('.bar-items').mouseenter(function(){
-    $(this).children('.dropdown-menu').show();
-    $('.drop-items').mouseenter(function(){
-      $(this).children('.second-dropdown').show();
-    });
+    $(this).children('.dropdown-cnt').show();
+    // $('.drop-items').mouseenter(function(){
+    //   $(this).children('.second-dropdown').show();
+    // });
 
   });
   $('.bar-items').mouseleave(function(){
-    $(this).children('.dropdown-menu').hide();
+    $(this).children('.dropdown-cnt').hide();
   });
 
 
