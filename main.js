28c421870dcdd91113e533c5cbd6928caa3d2f8e
DavideL90 <davide.lecci@icloud.com>
2018-04-05 16:20:02 +0200

    quasi completato

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
+  $('.bar-items').mouseenter(function(){
+    $(this).children('.dropdown-menu').show();
+    $('.drop-items').mouseenter(function(){
+      $(this).children('.second-dropdown').show();
+    });
+
+  });
+  $('.bar-items').mouseleave(function(){
+    $(this).children('.dropdown-menu').hide();
+  });
+
+
 });
