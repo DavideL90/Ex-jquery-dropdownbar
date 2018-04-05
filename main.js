159ca0130f8a93f2e8ad35f2c866ccee6695e2de
DavideL90 <davide.lecci@icloud.com>
2018-04-05 18:05:44 +0200

    manca da regolare l'apertura del 2nd menu

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
 
   $('.bar-items').mouseenter(function(){
     $(this).children('.dropdown-cnt').show();
-    // $('.drop-items').mouseenter(function(){
-    //   $(this).children('.second-dropdown').show();
-    // });
 
   });
   $('.bar-items').mouseleave(function(){
     $(this).children('.dropdown-cnt').hide();
   });
 
-
+  $('.drop-items').mouseenter(function(){
+      $(this).children('.second-dropdown-cnt').show();
+    });
+  $('.drop-items').mouseleave(function(){
+    $(this).children('.second-dropdown-cnt').hide();
+  });
 });
